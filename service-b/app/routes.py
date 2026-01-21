dfdc35d575ecd3016abcad72612adb7b27d4b816
yehuda linker <yehuda.linker@gmail.com>
2026-01-21 14:55:03 +0200

    good convert to dataframe

--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, HTTPException
-
+from serevice import create_data_frame,config_typs_db, create_temperature_category_colum, create_wind_status_colum
 
 router = APIRouter()
 
 
-@router.post("/")
-def hhhhhh():
-    try:
-        return {"meseg":"hii"}
-    
+@router.post("/clean")
+def Normalization_data_and_post_dataframe_for_servic_c(data):
+    try:    
+        df = create_data_frame(data)
+        df = config_typs_db(df)
+        df = create_temperature_category_colum(df)
+        df = create_wind_status_colum(df)
+        return {"meseg":df.to_json()}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
--- service-b/app/serevice.py
+++ service-b/app/serevice.py
@@ -1,65 +1,13 @@
-import pandas as ps
+import pandas as pd
 from pandas import DataFrame
-
-data = [
-  {
-    "timestamp": "2026-01-19T00:00:00",
-    "location_name": "London",
-    "country": "United Kingdom",
-    "latitude": 51.50853,
-    "longitude": -0.12574,
-    "temperature": 8.6,
-    "wind_speed": 3.9,
-    "humidity": 83
-  },
-  {
-    "timestamp": "2026-01-19T01:00:00",
-    "location_name": "London",
-    "country": "United Kingdom",
-    "latitude": 51.50853,
-    "longitude": -0.12574,
-    "temperature": 8.2,
-    "wind_speed": 3.4,
-    "humidity": 84
-  },
-  {
-    "timestamp": "2026-01-19T02:00:00",
-    "location_name": "London",
-    "country": "United Kingdom",
-    "latitude": 51.50853,
-    "longitude": -0.12574,
-    "temperature": 8.1,
-    "wind_speed": 3.1,
-    "humidity": 84
-  },
-  {
-    "timestamp": "2026-01-19T03:00:00",
-    "location_name": "London",
-    "country": "United Kingdom",
-    "latitude": 51.50853,
-    "longitude": -0.12574,
-    "temperature": 8.1,
-    "wind_speed": 12.6,
-    "humidity": 86
-  },
-  {
-    "timestamp": "2026-01-19T04:00:00",
-    "location_name": "London",
-    "country": "United Kingdom",
-    "latitude": 51.50853,
-    "longitude": -0.12574,
-    "temperature": 20.1,
-    "wind_speed": 3.9,
-    "humidity": 89
-  }]
-
-df= ps.DataFrame(data)
+import json
 
 def create_data_frame(data):
-    return ps.DataFrame(data)
+    data = json.loads(data)
+    return pd.DataFrame(data)
 
 def config_typs_db(df:DataFrame):
-    df["timestamp"]=df["timestamp"].astype("datetime64[ns]")
+    df["timestamp"]=pd.to_datetime(df["timestamp"])
     df["location_name"] = df["location_name"].astype("category")
     df["country"] = df["country"].astype("category")
     return df
